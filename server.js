5457f9b1ff6847a4aaefec3588ae527b66af07ce
fabianschu <48454910+fabianschu@users.noreply.github.com>
2020-04-10 21:48:12 +0200

    querying neo4j example db works

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const got = require('got');
 const express = require('express');
 const app = express();
-//const logger = require("morgan");
 const links = require('./routes/links');
 const crawl = require('./routes/crawl');
 const logger = require("morgan");
+const bodyParser = require("body-parser");
+const path = require('path');
+
+const neo4j = require('neo4j-driver');
 
 app.use(logger("dev"));
 
-app.get('/', function (req, res) {
-    return res.send('Hello world');
+const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123456'));
+const session = driver.session();
+
+app.get('/', (req, res) => {
+    session
+        .run("MATCH(n:Movie) RETURN n LIMIT 25")
+        .then(response => {
+            const movieArray = 
+            response.records.map(record => {
+                return {
+                    id: record._fields[0].identity.low,
+                    title: record._fields[0].properties.title
+                }
+            })
+            return res.json(movieArray);
+        })
+        .catch(err => console.log(err))
+    //return res.send('Hello world');
 });
 
 app.listen(process.env.PORT || 5000, () => console.log('listening on port '));
